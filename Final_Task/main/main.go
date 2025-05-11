6dac098f8afd97cf16003d5e9fa37f71dc2d7851
KhalidAskhab <120113799+KhalidAskhab@users.noreply.github.com>
2025-05-12 00:29:43 +0300

    Add files via upload

---
+++ Final_Task/main/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt
+	"fmt"
+ub.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/maxpa
+	"github.com/gorilla/mux"
+	"github.com/maxpawgdbs/yandex-go/handlers"
+	"github.com/maxpawgdbs/yandex-go/auth"
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+	"context"
+	"time"
+)
+
+func main() {
+
+	handlers.Initial()
+	
+	err := os.MkdirAll("database", os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	conn, err := sql.Open("sqlite3", "database/database.sql")
+	if err != nil {
+		os.Create("database/database.sql")
+		conn, err = sql.Open("sqlite3", "database/database.sql")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+	schemaSql := `
+	CREATE TABLE IF NOT EXISTS auth (
+    login    TEXT UNIQUE
+                  PRIMARY KEY
+                  NOT NULL,
+    password TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS expressions (
+    id     INTEGER PRIMARY KEY
+                   UNIQUE
+                   NOT NULL,
+    status TEXT    NOT NULL
+                   DEFAULT PENDING,
+    result NUMERIC NOT NULL
+);`
+	_, err = conn.Exec(schemaSql)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+		log.Fatalf("failed to init schema: %v", err)
+	}
+        log.Fatalf("failed to init schema: %v", err)
+    }
+
+	auth.InitAuth(conn)
+
+	log.Println("Starting Server")
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/register", auth.RegisterHandler).Methods("POST")
+	r.HandleFunc("/api/v1/login", auth.LoginHandler).Methods("POST")
+
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.Use(auth.JwtMiddleware)
+	api.HandleFunc("/calculate", handlers.CalculatorHandler)
+	api.HandleFunc("/expressions/{id}", handlers.ExpressionAnswer)
+	api.HandleFunc("/expressions", handlers.ExpressionsList)
+
+	r.HandleFunc("/internal/task", handlers.OrkestratorHandler)
+
+	http.ListenAndServe(":8080", r)
+}
